71f15e197948e7ee8ce456b4e2cf35a62de4b77c
gitjeki <dzaky230305@gmail.com>
2025-03-24 19:26:42 +0700

    test jaki

--- lib/main.dart
+++ lib/main.dart
@@ -16,3 +16,27 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'Halo, aku Jeki',
+              style: TextStyle(fontSize: 24),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
