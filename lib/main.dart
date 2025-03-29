81df479f531f5619664e9e80cd2bf8f313a91565
Xeyla Vithra Arfhina <xeyla@xeyla.xeyla>
2025-03-29 16:36:28 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -16,27 +16,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Main Screen'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Halo, aku Najma',
-              style: TextStyle(fontSize: 24),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
